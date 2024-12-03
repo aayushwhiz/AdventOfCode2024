61329d5dbee3146b1f5a423bc9c4aad8783f2f61
aayushwhiz <aayushwhiz@gmail.com>
2024-12-02 22:07:32 -0800

    day3 solved

---
+++ src/day3.rs
@@ -0,0 +1,81 @@
+use regex::Regex;
+use std::fs::File;
+use std::i32;
+use std::io::{self, BufRead};
+
+const PROBLEM_NUMBER: i32 = 2;
+
+pub fn run() -> io::Result<()> {
+    match PROBLEM_NUMBER {
+        1 => problem_1(),
+        2 => problem_2(),
+        _ => Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Invalid problem number",
+        )),
+    }
+}
+
+fn problem_1() -> io::Result<()> {
+    let input_file = "input/day3.txt";
+
+    let file = File::open(&input_file)?;
+    let reader = io::BufReader::new(file);
+
+    let mul_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut total_sum = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+
+        for capture in mul_regex.captures_iter(&line) {
+            let x: i32 = capture[1].parse().unwrap_or(0);
+            let y: i32 = capture[2].parse().unwrap_or(0);
+
+            total_sum += x * y;
+        }
+    }
+
+    println!("{}", total_sum);
+
+    Ok(())
+}
+
+fn problem_2() -> io::Result<()> {
+    let input_file = "input/day3.txt";
+
+    let file = File::open(&input_file)?;
+    let reader = io::BufReader::new(file);
+
+    let instruction_regex = Regex::new(r"(do\(\)|don't\(\)|mul\(\d+,\d+\))").unwrap();
+    let mul_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut mul_enabled = true;
+
+    let mut total_sum = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+
+        for capture in instruction_regex.find_iter(&line) {
+            let instruction = capture.as_str();
+
+            if instruction == "do()" {
+                mul_enabled = true;
+            } else if instruction == "don't()" {
+                mul_enabled = false;
+            } else if mul_enabled {
+                if let Some(mul_match) = mul_regex.captures(instruction) {
+                    let x: i32 = mul_match[1].parse().unwrap_or(0);
+                    let y: i32 = mul_match[2].parse().unwrap_or(0);
+
+                    total_sum += x * y;
+                }
+            }
+        }
+    }
+    println!("{}", total_sum);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io;
 mod day1;
 mod day2;
+mod day3;
 
-const DAY_NUMBER: i32 = 2;
+const DAY_NUMBER: i32 = 3;
 
 fn main() -> io::Result<()> {
     match DAY_NUMBER {
         1 => day1::run(),
         2 => day2::run(),
+        3 => day3::run(),
         _ => Err(io::Error::new(io::ErrorKind::Other, "Invalid day number")),
     }
 }
